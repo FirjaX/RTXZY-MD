4491fc58b9cd621e8a8d99c327dee2b7b2fd6b8f
FirjaX <107788805+FirjaX@users.noreply.github.com>
2024-07-28 00:35:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.owner = ['6281802626072']  
+global.mods = ['6281802626072'] 
+global.prems = ['6281802626072']
+global.nameowner = 'Rez'
+global.numberowner = '6281802626072' 
 global.mail = 'support@tioprm.eu.org' 
 global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
 global.instagram = 'https://instagram.com/prm2.0'
